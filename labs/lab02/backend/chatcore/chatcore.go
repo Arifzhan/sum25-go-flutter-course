d96f2a932eef049a6a71543e81f60991904feeb7
unknown <a.narimov@innopolis.university>
2025-07-03 13:32:31 +0300

    chacore.go fixed

--- labs/lab02/backend/chatcore/chatcore.go
+++ labs/lab02/backend/chatcore/chatcore.go
@@ -37,33 +37,33 @@ func NewBroker(ctx context.Context) *Broker {
 // Run starts the broker event loop
 func (b *Broker) Run() {
 	go func() {
+		defer close(b.done)
 		for {
 			select {
 			case <-b.ctx.Done():
-				close(b.done)
 				return
 			case msg := <-b.input:
-				if msg.Broadcast {
-					b.broadcastMessage(msg)
-				} else {
-					b.sendPrivateMessage(msg)
-				}
+				b.dispatchMessage(msg)
 			}
 		}
 	}()
 }
 
-// SendMessage sends a message into the broker input queue
+// SendMessage sends a message to the broker
 func (b *Broker) SendMessage(msg Message) error {
+	if b.ctx.Err() != nil {
+		return errors.New("broker is shutting down")
+	}
+
 	select {
 	case <-b.ctx.Done():
-		return errors.New("broker is shut down")
+		return errors.New("broker is shutting down")
 	case b.input <- msg:
 		return nil
 	}
 }
 
-// RegisterUser adds a user to the broker
+// RegisterUser adds a user and their receiving channel
 func (b *Broker) RegisterUser(userID string, recv chan Message) {
 	b.usersMutex.Lock()
 	defer b.usersMutex.Unlock()
@@ -77,28 +77,26 @@ func (b *Broker) UnregisterUser(userID string) {
 	delete(b.users, userID)
 }
 
-// sendPrivateMessage sends a message to a specific recipient
-func (b *Broker) sendPrivateMessage(msg Message) {
+// dispatchMessage delivers the message to appropriate recipients
+func (b *Broker) dispatchMessage(msg Message) {
 	b.usersMutex.RLock()
 	defer b.usersMutex.RUnlock()
-	if ch, ok := b.users[msg.Recipient]; ok {
-		select {
-		case ch <- msg:
-		default:
-			// Optional: handle full channel
-		}
-	}
-}
 
-// broadcastMessage sends a message to all users (including sender)
-func (b *Broker) broadcastMessage(msg Message) {
-	b.usersMutex.RLock()
-	defer b.usersMutex.RUnlock()
-	for _, ch := range b.users {
-		select {
-		case ch <- msg:
-		default:
-			// Optional: handle full channel
+	if msg.Broadcast {
+		for _, ch := range b.users {
+			select {
+			case ch <- msg:
+			default:
+				// drop message if channel is full
+			}
+		}
+	} else {
+		if ch, ok := b.users[msg.Recipient]; ok {
+			select {
+			case ch <- msg:
+			default:
+				// drop message if channel is full
+			}
 		}
 	}
 }
